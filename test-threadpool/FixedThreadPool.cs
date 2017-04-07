beda1f68a62f72e503e7ef90bc4583d9785e19b3
Antender <antender@live.ru>
2017-04-07 21:09:34 +0300

    Working program

---
+++ test-threadpool/FixedThreadPool.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace FixedThreadPool
+{
+    public class ThreadPool 
+    {
+        readonly object _locker;
+        readonly Thread[] _workers;
+        bool _stopped;
+        readonly Queue<ITask> _tasksHP;
+        readonly Queue<ITask> _tasksNP;
+        readonly Queue<ITask> _tasksLP;
+        uint _taskCount;
+        const int HighPriorityRespite = 3;
+        uint _HPRespiteCount;
+        
+        public ThreadPool(uint threadcount) 
+        {
+            _locker = this;
+            _workers = new Thread[threadcount];
+            for (int i = 0; i < threadcount; i++)
+            {
+                (_workers [i] = new Thread (Consume)).Start();
+            }
+            _tasksHP = new Queue<ITask>();
+            _tasksNP = new Queue<ITask>();
+            _tasksLP = new Queue<ITask>();
+            _taskCount = 0;
+            _HPRespiteCount = HighPriorityRespite;
+        }
+        
+        public bool Execute(ITask task, Priority priority)
+        {
+            if (task == null)
+            {
+                return false;
+            }
+            lock (_locker)
+            {
+                if (_stopped)
+                {
+                    return false;
+                }
+                switch (priority) 
+                {
+                    case Priority.High:
+                        _tasksHP.Enqueue(task);
+                        break;
+                    case Priority.Normal:
+                        _tasksNP.Enqueue(task);
+                        break;
+                    case Priority.Low:
+                        _tasksLP.Enqueue(task);
+                        break;
+                }
+                _taskCount++;
+                Monitor.Pulse (_locker);
+                return true;
+            }
+        }
+
+        public void Stop()
+        {
+            lock (_locker)
+            {
+                _stopped = true;
+                foreach (var worker in _workers)
+                {
+                    _tasksLP.Enqueue(null);
+                    _taskCount++;
+                }
+            }
+            foreach (var worker in _workers)
+            {
+                worker.Join();
+            }
+        }
+        
+
+        void Consume()
+        {
+            while (true)
+            {
+                ITask item;
+                lock (_locker)
+                {
+                    while (_taskCount == 0) 
+                    {
+                        Monitor.Wait (_locker);
+                    }
+                    _taskCount--;
+                    if (_tasksHP.Count > 0 && _HPRespiteCount > 0) {
+                        item = _tasksHP.Dequeue();
+                        _HPRespiteCount--;
+                    } else if (_tasksNP.Count > 0) {
+                        item = _tasksNP.Dequeue();
+                        _HPRespiteCount = HighPriorityRespite;
+                    } else {
+                        item = _tasksLP.Dequeue();
+                        if (item == null)
+                        {
+                            return; // This signals our exit.
+                        }
+                    }
+                }
+                item.Execute();                          
+            }
+        }
+    }
+    
+    public enum Priority 
+    {
+        Low, Normal, High
+    }
+    
+    public interface ITask 
+    {
+        void Execute();
+    }
+}
--- test-threadpool/Program.cs
+++ test-threadpool/Program.cs
@@ -1,15 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FixedThreadPool;
 
-namespace test_threadpool
+internal class Task : ITask
 {
-    class Program
+    readonly int _itemNumber;
+
+    public Task(int itemNumber)
     {
-        static void Main(string[] args)
+        _itemNumber = itemNumber;
+    }
+
+    public void Execute()
+    {
+        System.Threading.Thread.Sleep(1000);
+        Console.WriteLine(" Task" + _itemNumber);
+    }
+}
+
+internal class Program
+{
+    private static void Main()
+    {
+        var tp = new ThreadPool(2);
+
+        Console.WriteLine("Enqueuing 10 items...");
+
+        for (var i = 0; i < 10; i++)
         {
+            var t = new Task(i);
+            switch (i%3)
+            {
+                case 0:
+                    tp.Execute(t, Priority.High);
+                    break;
+                case 1:
+                    tp.Execute(t, Priority.Normal);
+                    break;
+                case 2:
+                    tp.Execute(t, Priority.Low);
+                    break;
+            }
         }
+
+        tp.Stop();
+        Console.WriteLine();
+        Console.WriteLine("Workers complete!");
+        Console.ReadLine();
     }
 }
